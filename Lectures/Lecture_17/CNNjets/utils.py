1da35bc38d7cab512df211a53c93b41d3b0aaaf6
Simon Badger <badger.simon@gmail.com>
2025-11-21 12:47:38 +0100

    fix CNN example (broken on OSX)

--- Lectures/Lecture_17/CNNjets/utils.py
+++ Lectures/Lecture_17/CNNjets/utils.py
@@ -1,4 +1,4 @@
-import uproot
+import uproot3 as uproot
 import numpy as np
 import awkward as ak
 
@@ -24,46 +24,69 @@ def to_np_array(ak_array, max_n=100, pad=0):
     return ak.fill_none(ak.pad_none(ak_array, max_n, clip=True, axis=-1), pad).to_numpy()
 
 
-def get_features_labels(file_name, features, spectators, labels, remove_mass_pt_window=True, entry_stop=None):
-    # load file
-    root_file = uproot.open(file_name, **get_file_handler(file_name))
-    tree = root_file['deepntuplizer/tree']
-    feature_array = tree.arrays(features,
-                                entry_stop=entry_stop,
-                                library='np')
-    spec_array = tree.arrays(spectators,
-                             entry_stop=entry_stop,
-                             library='np')
-    label_array_all = tree.arrays(labels,
-                                  entry_stop=entry_stop,
-                                  library='np')
+def get_features_labels(file_name, features, spectators, labels, entry_stop=None):
+    """
+    Load features, labels, and spectator variables from a local ROOT file (uproot3-compatible).
 
-    feature_array = np.stack([feature_array[feat] for feat in features], axis=1)
-    spec_array = np.stack([spec_array[spec] for spec in spectators], axis=1)
+    Parameters
+    ----------
+    file_name : str
+        Path to local ROOT file.
+    features : list of str
+        Branch names for features.
+    spectators : list of str
+        Branch names for spectator variables.
+    labels : list of str
+        Branch names for labels.
+    entry_stop : int or None
+        Maximum number of entries to read. If None, read all.
+
+    Returns
+    -------
+    feature_array : np.ndarray
+        Feature array of shape (n_events, n_features)
+    label_array : np.ndarray
+        Label array of shape (n_events, 2)
+    spec_array : np.ndarray
+        Spectator array of shape (n_events, n_spectators)
+    """
+
+    # load file
+    root_file = uproot.open(file_name)
+    tree = root_file['deepntuplizer/tree']
+
+    # read branches individually and slice to entry_stop
+    N = entry_stop if entry_stop is not None else None
+
+    feature_arrays = [tree.array(f)[:N] for f in features]
+    spec_arrays = [tree.array(s)[:N] for s in spectators]
+    label_arrays = [tree.array(l)[:N] for l in labels]
+
+    # stack arrays
+    feature_array = np.stack(feature_arrays, axis=1)
+    spec_array = np.stack(spec_arrays, axis=1)
+    label_array_all = {l: arr for l, arr in zip(labels, label_arrays)}
 
     njets = feature_array.shape[0]
 
+    # construct label array
     label_array = np.zeros((njets, 2))
-    label_array[:, 0] = label_array_all['sample_isQCD'] * (label_array_all['label_QCD_b'] +
-                                                           label_array_all['label_QCD_bb'] +
-                                                           label_array_all['label_QCD_c'] +
-                                                           label_array_all['label_QCD_cc'] +
-                                                           label_array_all['label_QCD_others'])
+    label_array[:, 0] = label_array_all['sample_isQCD'] * (
+        label_array_all['label_QCD_b'] +
+        label_array_all['label_QCD_bb'] +
+        label_array_all['label_QCD_c'] +
+        label_array_all['label_QCD_cc'] +
+        label_array_all['label_QCD_others']
+    )
     label_array[:, 1] = label_array_all['label_H_bb']
 
-    # remove samples outside mass/pT window
-    if remove_mass_pt_window:
-        feature_array = feature_array[(spec_array[:, 0] > 40) & (spec_array[:, 0] < 200) & (spec_array[:, 1] > 300) & (spec_array[:, 1] < 2000)]
-        label_array = label_array[(spec_array[:, 0] > 40) & (spec_array[:, 0] < 200) & (spec_array[:, 1] > 300) & (spec_array[:, 1] < 2000)]
-        spec_array = spec_array[(spec_array[:, 0] > 40) & (spec_array[:, 0] < 200) & (spec_array[:, 1] > 300) & (spec_array[:, 1] < 2000)]
-
     # remove unlabeled data
-    feature_array = feature_array[np.sum(label_array, axis=1) == 1]
-    spec_array = spec_array[np.sum(label_array, axis=1) == 1]
-    label_array = label_array[np.sum(label_array, axis=1) == 1]
+    mask = np.sum(label_array, axis=1) == 1
+    feature_array = feature_array[mask]
+    spec_array = spec_array[mask]
+    label_array = label_array[mask]
 
     return feature_array, label_array, spec_array
-
 
 def make_image(feature_array, n_pixels=224, img_ranges=[[-0.8, 0.8], [-0.8, 0.8]]):
     wgt = feature_array[:, 0]  # ptrel
